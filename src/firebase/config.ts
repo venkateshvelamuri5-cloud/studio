301c80c4eea696e452ea7510a583a73466e06073
Venkatesh velamuri <venkateshvelamuri5@gmail.com>
2026-02-14 11:51:27 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCskoUssIbWf39-gTSHPe9hwZk7wH1K

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDummyKey-For-Setup",
-  authDomain: "aago-app.firebaseapp.com",
-  projectId: "aago-app",
-  storageBucket: "aago-app.firebasestorage.app",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyCskoUssIbWf39-gTSHPe9hwZk7wH1Kbuw",
+  authDomain: "studio-8373328460-e5603.firebaseapp.com",
+  databaseURL: "https://studio-8373328460-e5603-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-8373328460-e5603",
+  storageBucket: "studio-8373328460-e5603.firebasestorage.app",
+  messagingSenderId: "989098027005",
+  appId: "1:989098027005:web:af7116e6fdd2e61dbb9b94"
 };
